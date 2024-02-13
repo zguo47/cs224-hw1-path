e375072cd2968348f89d3e9d07d9861a2b15422c
Shania <zixuan_guo@brown.edu--global user.name zguo47>
2024-02-13 04:12:53 -0500

    80 points

--- pathtracer.cpp
+++ pathtracer.cpp
@@ -8,6 +8,8 @@
 
 #include <util/CS123Common.h>
 
+#include <math.h>
+
 using namespace Eigen;
 
 PathTracer::PathTracer(int width, int height)
@@ -23,10 +25,10 @@ void PathTracer::traceScene(QRgb *imageData, const Scene& scene)
         #pragma omp parallel for
         for(int x = 0; x < m_width; ++x) {
             int offset = x + (y * m_width);
-            for (int i = 0; i < 40; i++){
+            for (int i = 0; i < settings.samplesPerPixel; i++){
                 intensityValues[offset] += tracePixel(x, y, scene, invViewMat);
             }
-            intensityValues[offset] /= 40.0f;
+            intensityValues[offset] /= settings.samplesPerPixel;
         }
     }
 
@@ -41,10 +43,10 @@ Vector3f PathTracer::tracePixel(int x, int y, const Scene& scene, const Matrix4f
 
     Ray r(p, d);
     r = r.transform(invViewMatrix);
-    return traceRay(r, scene);
+    return traceRay(r, scene, true);
 }
 
-Vector3f PathTracer::traceRay(const Ray& r, const Scene& scene)
+Vector3f PathTracer::traceRay(const Ray& r, const Scene& scene, bool count_emitted)
 {
     IntersectionInfo i;
     Ray ray(r);
@@ -57,13 +59,7 @@ Vector3f PathTracer::traceRay(const Ray& r, const Scene& scene)
        const tinyobj::real_t *d = mat.diffuse;//Diffuse color as array of floats
        const std::string diffuseTex = mat.diffuse_texname;//Diffuse texture name
        Vector3f kd;
-       if (d[0] < 0.02 && d[1] < 0.02 && d[2] < 0.02){
-           kd = Vector3f(1, 1, 1);
-       }else{
-           kd = Vector3f(d[0], d[1], d[2]);
-       }
-
-       Vector3f color(mat.emission[0], mat.emission[1], mat.emission[2]);
+       kd = Vector3f(d[0], d[1], d[2]);
 
        float epsilon = 0.001;
        Vector3f newRayDir;
@@ -74,10 +70,69 @@ Vector3f PathTracer::traceRay(const Ray& r, const Scene& scene)
        auto CTM = Quaternionf::FromTwoVectors(Vector3f(0.0f, 1.0f, 0.0f), i.object->getNormal(i));
        newRayDir = CTM * newRayDir;
 
-       if((static_cast<float>(rand()) / RAND_MAX) < settings.pathContinuationProb) {
+       Vector3f color = directLighting(r, scene, i);
+
+       if((static_cast<float>(rand()) / RAND_MAX) < settings.pathContinuationProb && !settings.directLightingOnly) {
            Ray newRay(i.hit + newRayDir * epsilon, newRayDir);
-           brdf = BRDF(kd);
-           color += traceRay(newRay, scene).cwiseProduct(brdf) * (newRayDir.dot(i.object->getNormal(i))) / (pdf * settings.pathContinuationProb);
+           brdf = BRDF(mat, r.d, i, newRayDir);
+           //mirror
+           if (mat.diffuse[0] < 0.02 && mat.diffuse[1] < 0.02 && mat.diffuse[2] < 0.02 &&
+               mat.specular[0] > 0.9 && mat.specular[1] > 0.9 && mat.specular[2] > 0.9){
+
+               Vector3f ks(mat.specular[0], mat.specular[1], mat.specular[2]);
+               Vector3f viewDir = - r.d.normalized();
+               Vector3f reflectedRay = 2 * viewDir.dot(i.object->getNormal(i)) * i.object->getNormal(i) - viewDir;
+               reflectedRay.normalize();
+               Ray newReflectedRay(i.hit + reflectedRay * epsilon, reflectedRay);
+               color += traceRay(newReflectedRay, scene, true).cwiseProduct(ks) / settings.pathContinuationProb;
+           //refraction
+           }else if (mat.diffuse[0] < 0.02 && mat.diffuse[1] < 0.02 && mat.diffuse[2] < 0.02 ){
+               Vector3f refnormal = i.object->getNormal(i);
+               float NdotI = refnormal.dot(r.d);
+               float etai = 1.0f;
+               float etat = mat.ior;
+               if (NdotI < 0){
+                   NdotI = -NdotI;
+               }else{
+                   refnormal = -refnormal;
+                   std::swap(etai, etat);
+               }
+               float eta = etai / etat;
+               float k = 1 - eta * eta * (1 - NdotI * NdotI);
+               float cosTheta_t = sqrt(k);
+               if (k < 0){
+                   Vector3f ks(mat.specular[0], mat.specular[1], mat.specular[2]);
+                   Vector3f viewDir = - r.d.normalized();
+                   Vector3f reflectedRay = 2 * viewDir.dot(i.object->getNormal(i)) * i.object->getNormal(i) - viewDir;
+                   reflectedRay.normalize();
+                   Ray newReflectedRay(i.hit + reflectedRay * epsilon, reflectedRay);
+                   color += traceRay(newReflectedRay, scene, true).cwiseProduct(ks) / settings.pathContinuationProb;
+               }else{
+                   float R0 = ((etai - etat) / (etai + etat)) * ((etai - etat) / (etai + etat));
+                   float R_thetai = R0 + (1 - R0) * (1 - NdotI) * (1 - NdotI) * (1 - NdotI) * (1 - NdotI) * (1 - NdotI);
+                   Vector3f refractedRay = eta * r.d + (eta * NdotI - cosTheta_t)  * refnormal;
+                   refractedRay.normalize();
+                   Ray newRefractedRay(i.hit + refractedRay * epsilon, refractedRay);
+
+                   Vector3f ks(mat.specular[0], mat.specular[1], mat.specular[2]);
+                   Vector3f viewDir = - r.d.normalized();
+                   Vector3f reflectedRay = 2 * viewDir.dot(i.object->getNormal(i)) * i.object->getNormal(i) - viewDir;
+                   reflectedRay.normalize();
+                   Ray newReflectedRay(i.hit + reflectedRay * epsilon, reflectedRay);
+                   if (((double) rand() / (RAND_MAX)) > R_thetai){
+                        color += traceRay(newRefractedRay, scene, true) / settings.pathContinuationProb;
+                   }else{
+                       color += traceRay(newReflectedRay, scene, true).cwiseProduct(ks) / settings.pathContinuationProb;
+                   }
+
+               }
+           }
+           else{
+               color += traceRay(newRay, scene, false).cwiseProduct(brdf) * (newRayDir.dot(i.object->getNormal(i))) / (pdf * settings.pathContinuationProb);
+           }
+       }
+       if (count_emitted){
+           color += Vector3f(mat.emission[0], mat.emission[1], mat.emission[2]);
        }
 
         return color;
@@ -86,8 +141,94 @@ Vector3f PathTracer::traceRay(const Ray& r, const Scene& scene)
     }
 }
 
-Vector3f PathTracer::BRDF(Vector3f &color) {
-    return color / M_PI;
+Vector3f PathTracer::BRDF(const tinyobj::material_t& mat, Vector3f inputRay, IntersectionInfo i, Vector3f newRayDir) {
+    Vector3f output;
+    Vector3f color(mat.diffuse[0], mat.diffuse[1], mat.diffuse[2]);
+    //glossy
+    if (mat.diffuse[0] > 0.4 && mat.diffuse[1] > 0.4 && mat.diffuse[2] > 0.4
+               && mat.specular[0] > 0.75 && mat.specular[1] > 0.75 && mat.specular[2] > 0.75){
+        int n = mat.shininess;
+        Vector3f ks(mat.specular[0], mat.specular[1], mat.specular[2]);
+        Vector3f viewDir = - inputRay.normalized();
+        Vector3f reflectedRay = 2 * viewDir.dot(i.object->getNormal(i)) * i.object->getNormal(i) - viewDir;
+        reflectedRay.normalize();
+        newRayDir.normalize();
+        output = ks * (n + 2)/(2 * M_PI) * pow((reflectedRay.dot(newRayDir)), n);
+    //diffuse
+    }else{
+        output = color / M_PI;
+    }
+
+    return output;
+}
+
+Vector3f PathTracer::directLighting(const Ray& r, const Scene& scene, IntersectionInfo i){
+    Vector3f directLight(0.0f, 0.0f, 0.0f); // Initialize direct light contribution to black
+    float area = 0.0f;
+
+    const std::vector<Triangle*>& emissives = scene.getEmissives();
+    for (Triangle* emissiveTriangle : emissives) {
+        // Calculate direction from point of intersection to light source
+        Vector3f samplePoint = samplePointOnTriangle(emissiveTriangle->getVertices());
+        area = triangleArea(emissiveTriangle->getVertices());
+
+        Vector3f lightDir = samplePoint - i.hit;
+        float distanceSquared = lightDir.squaredNorm();
+        lightDir.normalize();
+
+        // Check visibility (shadow ray)
+        Ray shadowRay(i.hit + i.object->getNormal(i) * 0.001f, lightDir);
+        IntersectionInfo shadowInfo;
+        if (scene.getIntersection(shadowRay, &shadowInfo) && shadowInfo.t < sqrt(distanceSquared) - 0.01f * sqrt(distanceSquared)) {
+            continue;
+        }
+
+        // Calculate the dot product between the light direction and the normal at the intersection
+        float NdotL = i.object->getNormal(i).dot(lightDir);
+        float NdotLPrime = emissiveTriangle->getNormal(samplePoint).dot(-lightDir);
+
+        Vector3f emittedLight = Vector3f(emissiveTriangle->getMaterial().emission[0],
+                                         emissiveTriangle->getMaterial().emission[1],
+                                         emissiveTriangle->getMaterial().emission[2]);
+
+        if (NdotL + 0.001f > 0) { // Add light contribution if the surface faces the light
+            const Triangle *t = static_cast<const Triangle *>(i.data);//Get the triangle in the mesh that was intersected
+            const tinyobj::material_t& mat = t->getMaterial();
+            if (mat.diffuse[0] > 0.02 && mat.diffuse[1] > 0.02 && mat.diffuse[2] > 0.02){
+                directLight += emittedLight.cwiseProduct(BRDF(mat, r.d, i, lightDir)) * NdotL * NdotLPrime * area / (distanceSquared);
+            }
+        }
+    }
+
+    return directLight;
+}
+
+Vector3f PathTracer::samplePointOnTriangle(const Vector3<Vector3f>& vertices) {
+    // Generate two random numbers
+    float r1 = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    float r2 = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+
+    // Barycentric coordinates for uniform triangle sampling
+    float sqrtR1 = sqrt(r1);
+    float u = 1.0f - sqrtR1;
+    float v = r2 * sqrtR1;
+
+    // Calculate the sampled point on the triangle
+    return vertices[0] * u + vertices[1] * v + vertices[2] * (1.0f - u - v);
+}
+
+float PathTracer::triangleArea(const Eigen::Vector3<Eigen::Vector3f>& vertices) {
+    Eigen::Vector3f v1 = vertices[0];
+    Eigen::Vector3f v2 = vertices[1];
+    Eigen::Vector3f v3 = vertices[2];
+
+    Eigen::Vector3f edge1 = v2 - v1;
+    Eigen::Vector3f edge2 = v3 - v1;
+
+    Eigen::Vector3f crossProduct = edge1.cross(edge2);
+    float area = 0.5f * crossProduct.norm();
+
+    return area;
 }
 
 std::pair<Vector3f, float> PathTracer::sampleNextDir() {
@@ -128,6 +269,6 @@ void PathTracer::toneMap(QRgb *imageData, std::vector<Vector3f> &intensityValues
                 );
         }
     }
-    outputPFM("/Users/shania/cs2240/path-zguo47/student_outputs/milestone/cornell_box_milestone.pfm", 512, 512, intensityValues);
+    // outputPFM("/Users/shania/cs2240/path-zguo47/student_outputs/final/cornell_box_full_lighting.pfm", 512, 512, intensityValues);
 
 }
--- pathtracer.h
+++ pathtracer.h
@@ -20,17 +20,19 @@ public:
     void traceScene(QRgb *imageData, const Scene &scene);
     Settings settings;
 
-    const int RECURSION_DEPTH = 15;
-
 private:
     int m_width, m_height;
 
     void toneMap(QRgb *imageData, std::vector<Eigen::Vector3f> &intensityValues);
 
     Eigen::Vector3f tracePixel(int x, int y, const Scene &scene, const Eigen::Matrix4f &invViewMatrix);
-    Eigen::Vector3f traceRay(const Ray& r, const Scene &scene);
-    Eigen::Vector3f BRDF(Eigen::Vector3f &color);
+    Eigen::Vector3f traceRay(const Ray& r, const Scene &scene, bool count_emitted);
+    Eigen::Vector3f BRDF(const tinyobj::material_t&, Eigen::Vector3f inputRay, IntersectionInfo i, Eigen::Vector3f newRayDir);
     std::pair<Eigen::Vector3f, float> sampleNextDir();
+    Eigen::Vector3f directLighting(const Ray& r, const Scene& scene, IntersectionInfo i);
+    float triangleArea(const Eigen::Vector3<Eigen::Vector3f>& vertices);
+    Eigen::Vector3f samplePointOnTriangle(const Eigen::Vector3<Eigen::Vector3f>& vertices);
+    void fresnel(const Eigen::Vector3f I, const Eigen::Vector3f normal, const float ior, float kr);
 };
 
 #endif // PATHTRACER_H
